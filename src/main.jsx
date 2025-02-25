c44cc4be98421568a9e3d6f918dd7b41d80f417f
jay kumar <kumarprince13833@gmail.com>
2025-02-26 01:35:37 +0530

    somechanges

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/ReactAuth/">
       <App />
       <Toaster></Toaster>
     </BrowserRouter>
